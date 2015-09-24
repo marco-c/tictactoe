4efa8be4894ada4c9b575af748967f8ebfc676a0
Marco Castelluccio <mar.castelluccio@studenti.unina.it>
2015-09-24 18:56:00 +0200

    Use oghliner to generate service worker

--- gulpfile.js
+++ gulpfile.js
@@ -1,4 +1,21 @@
 var gulp = require('gulp');
+var oghliner = require('oghliner');
 
 gulp.task('default', function() {
+});
+
+gulp.task('offline', function(callback) {
+  oghliner.offline({
+    rootDir: '.',
+    fileGlobs: [
+      'tictactoe.html',
+      'js/**/*.js',
+    ],
+  }, callback);
+});
+
+gulp.task('deploy', function(callback) {
+  oghliner.deploy({
+    rootDir: '.',
+  }, callback);
 });
