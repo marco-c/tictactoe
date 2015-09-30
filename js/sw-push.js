caf9df55a521269f123b14ba984f241444a61d10
Travis CI <mar.castelluccio@studenti.unina.it>
2015-09-30 16:22:00 +0000

    Updates

--- js/sw-push.js
+++ js/sw-push.js
@@ -1,6 +1,11 @@
 var port;
 
 self.addEventListener('push', function(event) {
+  event.waitUntil(self.registration.showNotification('TicTacToe', {
+    body: 'It\'s your turn!',
+    tag: 'tictactoe',
+  }));
+
   port.postMessage(event.data.json());
 });
 
