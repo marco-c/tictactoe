ada85d1a16ef07d4d10c2d7413839148de7cdfa9
Travis CI <mar.castelluccio@studenti.unina.it>
2015-09-30 16:23:20 +0000

    Updates

--- js/sw-push.js
+++ js/sw-push.js
@@ -1,5 +1,13 @@
 var port;
 
+self.addEventListener('notificationclick', function(event) {
+  event.waitUntil(clients.matchAll().then(function(clientList) {
+    if (clientList.length > 0) {
+      return clientList[0].focus();
+    }
+  }));
+});
+
 self.addEventListener('push', function(event) {
   event.waitUntil(self.registration.showNotification('TicTacToe', {
     body: 'It\'s your turn!',
